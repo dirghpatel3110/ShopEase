e563cc2d65367482440d4b18c33e3a3372b86efa
dirghpatel3110 <dirghpatel3110@gmail.com>
2024-12-18 00:38:54 -0600

    Docker Setup done

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(
   
 app.use(express.json()); 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
